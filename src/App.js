2b42f2cca1b21ede016a30969c0d77ce9496a861
Bruno Paris <brunojparisz@gmail.com>
2022-02-07 00:03:05 -0300

    Connection to API through Axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from './components/Card';
-
-
+import axios from "axios";
 
 function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    axios.get(`https://pixabay.com/api/?key=25586356-b53de271deeb82ccc4ddccaa8&q=${term}&image_type=photo`)
+      .then(function (response) {
+        // handle success
+        console.log(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, []);
   return (
-    <div>
+    <div className= "px-6">
       <Card/>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,28 @@ import React from "react";
 const Card = () => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
       <div className="px-6 py-4">
-        <div className="font-bold text-black text-xl">photo by bruno</div>
+        <div className="font-bold text-black text-xl">Bruno Paris </div>
       </div>
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div>
+          <ul>
+              <li>
+                  <strong>Views: </strong>
+                  1231
+              </li>
+              <li>
+                  <strong>Downloads: </strong>
+                  342                    
+              </li>
+              <li>
+                  <strong>Likes: </strong>
+                  212
+              </li>
+
+          </ul>
+      </div>
+      
     </div>
   );
 };
